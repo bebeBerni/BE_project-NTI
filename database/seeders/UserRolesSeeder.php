e7a25bae236529e1cc29d19a99cab8eeb096e39d
Khawakii <podachloe5@gmail.com>
2026-04-22 12:09:03 +0200

    mar felismerte +empty spaces removed

--- database/seeders/UserRolesSeeder.php
+++ database/seeders/UserRolesSeeder.php
@@ -1,10 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Role;
 
