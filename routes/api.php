32c898e75d9190ac8dd7e072eec28f1195e1455d
bebeBerni <bernadett.baloghova@student.ukf.sk>
2026-05-15 11:50:00 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommissionController;
@@ -25,18 +26,21 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/ping-db', function () {
     return response()->json([
-        'status' => 'ok'
+        'status' => 'ok',
     ]);
 });
 
-// ✅ IDE TEDD (AUTHON KÍVÜL)
-Route::post('/debug', function (Illuminate\Http\Request $request) {
+Route::post('/change-password', [AuthController::class, 'changePassword']);
+
+Route::post('/debug', function (Request $request) {
     return response()->json([
         'all' => $request->all(),
         'json' => $request->json()->all(),
-        'headers' => $request->headers->all()
+        'headers' => $request->headers->all(),
     ]);
 });
+
+
 /*
 |--------------------------------------------------------------------------
 | AUTHENTICATED ROUTES
@@ -45,41 +49,139 @@ Route::post('/debug', function (Illuminate\Http\Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    /*
+    |--------------------------------------------------------------------------
+    | AUTH
+    |--------------------------------------------------------------------------
+    */
+
     Route::get('/me', function (Request $request) {
         return response()->json([
-            'user' => $request->user()?->load('roles')
+            'user' => $request->user()?->load('roles'),
         ]);
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/logout-all', [AuthController::class, 'logoutAll']);
+    Route::post('/change-password', [AuthController::class, 'changePassword']);
 
-    Route::post('/change-password', function (Request $request) {
-        return app(AuthController::class)->changePassword($request);
-    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | DEBUG / TEST
+    |--------------------------------------------------------------------------
+    */
 
     Route::get('/test-role', function (Request $request) {
-        return [
+        return response()->json([
             'email' => $request->user()->email,
             'roles' => $request->user()->roles->pluck('name'),
-        ];
+        ]);
     });
-Route::get('/auth-debug', function (Request $request) {
-    return [
-        'bearer_token' => $request->bearerToken(),
-        'user' => $request->user(),
-        'auth_check' => auth()->check(),
-    ];
-})->middleware('auth:sanctum');
+
+    Route::get('/auth-debug', function (Request $request) {
+        return response()->json([
+            'bearer_token' => $request->bearerToken(),
+            'user' => $request->user(),
+            'auth_check' => auth()->check(),
+        ]);
+    });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | STUDENTS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('students', StudentController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | MENTORS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('mentors', MentorController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | TEAMS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('teams', TeamController::class);
+
+    Route::post('/teams/{team}/add-member', [TeamController::class, 'addMember']);
+    Route::delete('/teams/{team}/remove-member/{student}', [TeamController::class, 'removeMember']);
+    Route::post('/teams/{team}/activate', [TeamController::class, 'activate']);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROJECTS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('projects', ProjectController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROJECT APPLICATIONS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('project-applications', ProjectApplicationController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROJECT ASSIGNMENTS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('project-assignments', ProjectAssignmentController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROJECT HISTORIES
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('project-histories', ProjectHistoryController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | COMMISSIONS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('commissions', CommissionController::class);
+
+    Route::get('/commissions/{commission}/members', [CommissionController::class, 'members']);
+    Route::post('/commissions/{commission}/members', [CommissionController::class, 'addMember']);
+    Route::delete('/commissions/{commission}/members/{user}', [CommissionController::class, 'removeMember']);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | DECISIONS
+    |--------------------------------------------------------------------------
+    */
+
     Route::apiResource('decisions', DecisionController::class);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | CATEGORIES
+    |--------------------------------------------------------------------------
+    */
 
     Route::get('/categories', [CategoryController::class, 'index']);
 
