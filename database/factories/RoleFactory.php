c906e4958a975106e7311feadbf8a656b43f6a46
bebeBerni <bernadett.baloghova@student.ukf.sk>
2026-05-24 16:59:21 +0200

    Update RoleFactory.php

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -18,7 +18,7 @@ class RoleFactory extends Factory
     public function definition(): array
     {
         return [
-               'name' => fake()->randomElement(['admin', 'user', 'mentor']),
+               'name' => fake()->randomElement(['admin', 'user', 'mentor','company']),
         ];
     }
 }
